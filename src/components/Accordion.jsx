c95ea7bf842fd5a7e7d76f4dcf88fd3d62e60055
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-05 16:25:20 +0700

    add accordion component

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,12 @@
+export default function Accordion({ items }) {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
+}
