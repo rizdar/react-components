0de952a439898d4fbb2a5e10da273d2dbe4325a2
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-06 10:41:51 +0700

    clear the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,5 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './components/Button';
-
 function App() {
-  return (
-    <div>
-      <div>
-        <Button primary rounded outline>
-          <GoBell />
-          Click Me
-        </Button>
-      </div>
-      <div>
-        <Button className="mb-5" success onClick={() => console.log('Click..!!')}>
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>See Deal</Button>
-      </div>
-      <div>
-        <Button danger>
-          <GoCloudDownload />
-          Hide ads
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded>
-          <GoDatabase />
-          Secondary
-        </Button>
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
