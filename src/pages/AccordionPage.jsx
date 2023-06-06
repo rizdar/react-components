2d1766ec248171475513ef2778f0b0292b5bd458
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-06 10:39:47 +0700

    add accordion page component

---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,23 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'lkjakjkd',
+      label: 'Can I use React on a project ?',
+      content: 'You can use react on any project you want',
+    },
+    {
+      id: 'jsjkkskjdjs',
+      label: 'Can I use Javascript on a project ?',
+      content: 'You can use react on any project you want',
+    },
+    {
+      id: 'skskksdlld',
+      label: 'Can I use CSS on a project ?',
+      content: 'You can use react on any project you want',
+    },
+  ];
+  return <Accordion items={items} />;
+}
+export default AccordionPage;
