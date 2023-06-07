feaba3571d023e2d14b401dd02a35d71d872e67d
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-07 21:09:01 +0700

    dropdown done

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
 export default function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handler, true);
+    return () => {
+      document.removeEventListener('click', handler);
+      console.log('clean up');
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => setIsOpen(!currentIsOpen));
@@ -14,19 +34,19 @@ export default function Dropdown({ options, value, onChange }) {
 
   const renderedOptions = options.map((option) => {
     return (
-      <div className="hover:bg-sky-50 cursor-pointer p-3" onClick={() => handleOptioinClick(option)} key={option.value}>
+      <div className="hover:bg-sky-100 cursor-pointer p-3" onClick={() => handleOptioinClick(option)} key={option.value}>
         {option.label}
       </div>
     );
   });
 
   return (
-    <div className="w-48 relative">
-      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
+    <div ref={divEl} className="w-48 relative">
+      <Panel classNamesProps="flex justify-between items-center cursor-pointer" onClick={handleClick}>
         {value?.label || 'Select...'}
         <GoChevronDown className="text-lg" />
-      </div>
-      {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 }
