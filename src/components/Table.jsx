1e9e34fb75ee374a4d3d8410a306919629b085c8
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-11 20:39:54 +0700

    add table page and component

---
+++ src/components/Table.jsx
@@ -0,0 +1,29 @@
+export default function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr key={keyFn(rowData)} className="border-b">
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
+}
